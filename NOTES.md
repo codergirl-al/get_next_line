a794dfdec54cf2b4a4aef575aedfbac5d530e64e
Albina Peposhi <apeposhi@1-E-9.42heilbronn.de>
2023-01-18 14:03:05 +0100

    Markdown fix

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,6 @@
 ### What are static variables?
-
+<details>
+<summary> Static variables are variables initialized only once... </summary>
 - Static variables are variables initialized only once. The compiler persists with the variable until the end of the program. Static variables can be defined inside or outside the function. They are local to the block. The default value of static variables is zero and are alive until the execution of the program.
 
 ```C
@@ -31,4 +32,4 @@ int main()
 - In C, static variables can only be initialized using constant literals.
 - Static global variables and functions are also possible in C/C++. The purpose of these is to limit the scope of a variable or function to a file.
 - Static variables should not be declared inside structures.
-
+</details>
--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/16 17:42:52 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/01/16 17:42:53 by apeposhi         ###   ########.fr       */
+/*   Created: 2023/01/16 17:43:05 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/01/18 13:50:00 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+# endif

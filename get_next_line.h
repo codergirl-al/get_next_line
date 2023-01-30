b6250a55219bdf2fe390125853758725285384be
Albina Peposhi <apeposhi@3-F-6.42heilbronn.de>
2023-01-30 13:58:49 +0100

    Added header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,25 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 17:43:05 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/01/18 13:50:00 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/01/30 13:50:46 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
 # endif
+
 # include <stdlib.h>
 # include <unistd.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 # endif

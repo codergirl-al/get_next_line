a7fce63d84c6a4335b61c46a52a4f9987855fa92
Albina Peposhi <apeposhi@3-F-6.42heilbronn.de>
2023-01-30 17:31:03 +0100

    Added changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,40 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 17:43:02 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/01/19 13:49:45 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/01/30 17:30:57 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_line(int fd, char **buff, char *read)
+{
+	//in  developeplopeleoplement
+}
+
+void	free_space(char **ptr_to_free)
+{
+	if (*ptr_to_free != NULL)
+	{
+		free(*ptr_to_free);
+		ptr_to_free = NULL;
+	}
+}
+
 char	*get_next_line(int fd)
 {
-	char		*buffer;
-	static char	*svar;
-	int			i;
+	static char		*buff = NULL;
+	char			*read;
+	char			*res;
 
-	i = 0;
-	while (!i)
-	{
-		read()
-	}
-
-	
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > BUFFER_SIZE)
+		return (NULL);
+	read = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!read)
+		return (NULL);
+	if (!buff[fd])
+		buff[fd] = ft_strdup("");
+	res = get_line(fd, &buff, read);
+	free_space(&read);
+	return (res);	
 }
